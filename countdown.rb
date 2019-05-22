8771c98b25e0b53f1de78e62a9f5740bf87e8f0d
Brendan Robinson <brendanr425@gmail.com>
2019-05-22 17:21:23 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
 #write your code here
 
-def countdown
+def countdown(num)
+  counter = num
+  while counter > 0
+    puts "#{num} SECOND(S)!"
+    counter -= 1
+  end
+  puts "HAPPY NEW YEAR!"
 end
